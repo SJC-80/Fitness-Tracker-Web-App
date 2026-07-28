6c858437b510f04b11e552e56ece7436ab23277c
Sebz <sebajose14@gmail.com>
2026-07-28 13:33:48 -0400

    developed welcome page

--- FitnessApp/src/App.vue
+++ FitnessApp/src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
+  import Welcome from './components/pages/Welcome.vue'
 </script>
 
 <template>
-
+ <div>
+    <h1 class ="text-gradient">Fitness App</h1>
+ </div>
+ <Welcome></Welcome>
 </template>
 
 <style scoped>
+
 </style>
