22df019440c2f29c13f26e34c6942e9d6cc39198
Sebz <sebajose14@gmail.com>
2026-07-30 19:30:04 -0400

    Developed the Workout.vue page

--- FitnessApp/src/App.vue
+++ FitnessApp/src/App.vue
@@ -1,12 +1,21 @@
 <script setup>
   import Welcome from './components/pages/Welcome.vue'
+  import Layout from './components/layouts/Layout.vue'
+  import Workout from './components/pages/Workout.vue'
+  import Dashboard from './components/pages/Dashboard.vue'
+  const selectedDisplay = 3
 </script>
 
 <template>
- <div>
-    <h1 class ="text-gradient">Fitness App</h1>
- </div>
- <Welcome></Welcome>
+  <Layout>
+    <!-- PAGE #1 -->
+    <Welcome v-if="selectedDisplay == 1"/>
+    <!-- PAGE #2 -->
+    <Dashboard v-if="selectedDisplay == 2"/>
+     <!-- PAGE #3 -->
+    <Workout v-if="selectedDisplay == 3"/>
+  </Layout>
+
 </template>
 
 <style scoped>
---
+++ FitnessApp/src/components/pages/Dashboard.vue
@@ -0,0 +1,48 @@
+<script setup>
+    import { gymHealthFacts } from '../../utils'
+    import Grid from '../Grid.vue'
+    console.log(gymHealthFacts)
+    const randomNumber = Math.floor(Math.random() * gymHealthFacts.length)
+    const FactofTheDay = gymHealthFacts[randomNumber]
+    console.log(FactofTheDay)
+</script>
+
+<template>
+    <section id="dahsboard">
+        <div class="card tip-container">
+            <h2>Welcome Smoldier</h2>
+            <div>
+                <p class="tip"><strong>Daily Tip</strong><br/>{{ FactofTheDay }} </p>
+            </div>
+            <button>Start workout &rarr;</button>
+        </div>
+        <Grid/>
+    </section>
+</template>
+
+<style scoped>
+    .tip-container,
+    .tip-container div,
+    #dashboard {
+        display: flex;
+    }
+
+    .tip-container,
+    #dashboard {
+        flex-direction: column;
+    }
+
+    #dashboard {
+        gap: 2rem;
+    }    
+
+    .tip-container {
+        gap: 0.5rem;
+    }
+
+    @media (min-width: 640px){
+        .tip-container{
+            gap: 1rem;
+        }
+    }
+</style>
