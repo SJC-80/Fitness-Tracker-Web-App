07a05ca2fba0b7dc59467315f5b885bec03e08ef
Sebz <sebajose14@gmail.com>
2026-08-12 15:28:44 -0400

    Made further changes regarding Workout and Portal

--- FitnessApp/src/components/pages/Workout.vue
+++ FitnessApp/src/components/pages/Workout.vue
@@ -1,12 +1,23 @@
 <script setup>
-    import { workoutProgram } from '../../utils'
+    import { workoutProgram, exerciseDescriptions } from '../../utils'
     import Portal from '../Portal.vue'
     const selectedWorkout = 4
     const { workout, warmup } = workoutProgram[selectedWorkout]
+    let selectedExercise = null
+    const exerciseDescription = exerciseDescriptions[selectedExercise]
 </script>
 
 <template>
-    <Portal></Portal>
+    <Portal v-if="selectedExercise">
+        <div class="exercise-description">
+            <h3>{{ selectedExercise }}</h3>
+             <small>Description</small>
+             <div>
+                 <p>{{ exerciseDescription }}</p>
+             </div>
+             <button> Close <i class="fa-solid fa-xmark"></i></button>
+        </div>
+    </Portal>
     <section id="workout-card">
         <div class="plan-card card">
             <div class="plan-card-header">
@@ -134,4 +145,13 @@
     padding-left: 0.5rem;
 }
 
+.exercise-description{
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+}
+
+.exercise-description button i{
+    padding-left: 0.5rem;
+}
 </style>
